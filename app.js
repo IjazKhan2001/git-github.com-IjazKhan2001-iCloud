ba04d31fe1f4c5c80b9f200367124ce665e4e96f
IjazKhan2001 <101605490+IjazKhan2001@users.noreply.github.com>
2022-08-15 14:43:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active');
 });
+
+const toggle = document.getElementById("toggleDark");
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s'
+    }
+})
